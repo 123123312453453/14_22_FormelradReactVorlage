684856c8c242a6ba53111a5d10ef4601b3592bad
Tobias11111010110 <tobias.klingler@gmx.ch>
2024-06-04 12:00:35 +0200

    A clear handler is required.

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -41,6 +41,10 @@ export default function Formelrad() {
             setValues(values => ({...values, p: values.u * values.i}));
         }
     }
+    const handleClear = (event) => {
+        event.preventDefault();
+        console.log("handleClear");
+    }
 
     return (
         <>
@@ -55,6 +59,7 @@ export default function Formelrad() {
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <InputField color={"black"} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
                     <button type="submit">Calculate</button>
+                    <button style={{margin:10}} onClick={handleClear}>Clear</button>
                 </form>
             </section>
         </>
